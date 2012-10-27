d932fd7cffab982f1af03ddca9ad424c47592094
Nicolas Bareil <nico@chdir.org>
2012-10-27 18:40:17 +0200

    Adding copyright blabla

--- pdfviewer.c
+++ pdfviewer.c
@@ -1,3 +1,17 @@
+/*
+ *
+ * Copyright © 2012  Nicolas Bareil <nico@chdir.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
